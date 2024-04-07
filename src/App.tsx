896f1df51b3078e95847e66af6456560c9bdb258
Karol Zając <karolzajac.0407@gmail.com>
2024-04-07 15:24:20 +0200

    simple styling top panel

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import CitiesGrid from "./components/CitiesGrid";
 
 function App() {
   return (
-    <div className="">
-      test
+    <div className="main-bg">
+      <h1 className="italic text-[2rem] max-w-[50rem] mx-auto px-[0.3rem] py-[0.5rem] flex flex-wrap ">
+        <div className="text-left">The app shows </div>
+        <span className=" not-italic font-extrabold text-[2em] w-full text-center">
+          Live Air Condition
+        </span>{" "}
+        <div className=" w-full text-right ">in capitals around the Wolrd </div>
+      </h1>
       <CitiesGrid />
     </div>
   );
--- src/components/CitiesGrid.tsx
+++ src/components/CitiesGrid.tsx
@@ -3,15 +3,14 @@ import { CityDataContext } from "../context/CityContext";
 import CityTile from "./CityTile";
 import { AirConditionDataContext } from "../context/AirConditionContext";
 import { CityDTO } from "../types/City";
-import "react-dropdown/style.css";
 import Select from "react-select";
 import { useSort } from "../hooks/useSort";
 import { useFilter } from "../hooks/useFilter";
 
 const CitiesGrid = () => {
-  const data = useContext(CityDataContext);
-  const condition = useContext(AirConditionDataContext);
-  const [filters, setFilters] = useState<CityDTO[] | null>([]);
+  const citiesData = useContext(CityDataContext);
+  const conditionData = useContext(AirConditionDataContext);
+  const [filteredData, setFilteredData] = useState<CityDTO[] | null>([]);
 
   const {
     country,
@@ -21,38 +20,62 @@ const CitiesGrid = () => {
     selectedOptions,
     setSelectedOptions,
     options,
-  } = useFilter({ setFilters, condition, data });
+  } = useFilter({
+    setFilters: setFilteredData,
+    condition: conditionData,
+    data: citiesData,
+  });
   const { sortOptions, setSortOption } = useSort({
-    filters,
-    condition,
-    data,
-    setFilters,
+    filters: filteredData,
+    condition: conditionData,
+    data: citiesData,
+    setFilters: setFilteredData,
   });
   return (
     <>
-      <div className="bg-red-200">
-        <input type="text" value={city} onChange={handleCityChange} />
-        <input
-          type="text"
-          placeholder="country"
-          value={country}
-          onChange={handleCountryChange}
-        />
-        <Select
-          defaultValue={selectedOptions}
-          onChange={setSelectedOptions}
-          options={options}
-          isMulti
-        />
+      <div className="flex  w-full gap-[0.2rem] p-[0.5rem] flex-col md:flex-row">
+        <div className="bg-black/70 backdrop-blur-md  flex flex-col  gap-[0.2rem] flex-1 p-[2rem]">
+          <h1 className="text-[1.5rem] pb-[0.3rem] text-white">Type your filters:</h1>
+          <div className="flex w-full gap-[0.2rem]">
+            <input
+              className="flex-1 py-[0.4rem] px-[0.5rem]"
+              type="text"
+              placeholder="City"
+              value={city}
+              onChange={handleCityChange}
+            />
+            <input
+              className="flex-1 py-[0.4rem] px-[0.5rem]"
+              type="text"
+              placeholder="Country"
+              value={country}
+              onChange={handleCountryChange}
+            />
+          </div>
+          <Select
+            placeholder="Air condition "
+            defaultValue={selectedOptions}
+            onChange={setSelectedOptions}
+            options={options}
+            isMulti
+          />
+        </div>
+        <div className="w-full h-[0.2rem] bg-black/20 md:hidden" />
+        <div className="bg-black/20 backdrop-blur-md flex flex-col  gap-[0.2rem] flex-1 p-[2rem]">
+          <h1 className="text-[1.5rem] pb-[0.3rem]">Type your filters:</h1>
+          <Select
+            className="mt-auto"
+            options={sortOptions}
+            onChange={setSortOption}
+          />
+        </div>
       </div>
-      <div>
-        sorts:
-        <Select options={sortOptions} onChange={setSortOption} />
-      </div>
-      <div className="grid grid-cols-5 gap-[0.2rem]">
-        {filters?.map((city, index) => (
+      <div className="grid grid-cols-5 gap-[1.2rem]">
+        {filteredData?.map((city, index) => (
           <CityTile
-            airCondition={condition?.[data?.indexOf(city) ?? 0] ?? null}
+            airCondition={
+              conditionData?.[citiesData?.indexOf(city) ?? 0] ?? null
+            }
             city={city}
             key={city.country}
           />
